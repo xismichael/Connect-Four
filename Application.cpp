7da21ff71281e6a54d7d6294a16955fb59632195
gdevine-ucsc <gdevine@ucsc.edu>
2025-10-19 21:05:57 -0700

    first commit

---
+++ Application.cpp
@@ -0,0 +1,93 @@
+#include "Application.h"
+#include "imgui/imgui.h"
+#include "classes/TicTacToe.h"
+#include "classes/Checkers.h"
+#include "classes/Othello.h"
+
+namespace ClassGame {
+        //
+        // our global variables
+        //
+        Game *game = nullptr;
+        bool gameOver = false;
+        int gameWinner = -1;
+
+        //
+        // game starting point
+        // this is called by the main render loop in main.cpp
+        //
+        void GameStartUp() 
+        {
+            game = nullptr;
+        }
+
+        //
+        // game render loop
+        // this is called by the main render loop in main.cpp
+        //
+        void RenderGame() 
+        {
+                ImGui::DockSpaceOverViewport();
+
+                //ImGui::ShowDemoWindow();
+
+                ImGui::Begin("Settings");
+
+                if (gameOver) {
+                    ImGui::Text("Game Over!");
+                    ImGui::Text("Winner: %d", gameWinner);
+                    if (ImGui::Button("Reset Game")) {
+                        game->stopGame();
+                        game->setUpBoard();
+                        gameOver = false;
+                        gameWinner = -1;
+                    }
+                }
+                if (!game) {
+                    if (ImGui::Button("Start Tic-Tac-Toe")) {
+                        game = new TicTacToe();
+                        game->setUpBoard();
+                    }
+                    if (ImGui::Button("Start Checkers")) {
+                        game = new Checkers();
+                        game->setUpBoard();
+                    }
+                    if (ImGui::Button("Start Othello")) {
+                        game = new Othello();
+                        game->setUpBoard();
+                    }
+                } else {
+                    ImGui::Text("Current Player Number: %d", game->getCurrentPlayer()->playerNumber());
+                    ImGui::Text("Current Board State: %s", game->stateString().c_str());
+                }
+                ImGui::End();
+
+                ImGui::Begin("GameWindow");
+                if (game) {
+                    if (game->gameHasAI() && (game->getCurrentPlayer()->isAIPlayer() || game->_gameOptions.AIvsAI))
+                    {
+                        game->updateAI();
+                    }
+                    game->drawFrame();
+                }
+                ImGui::End();
+        }
+
+        //
+        // end turn is called by the game code at the end of each turn
+        // this is where we check for a winner
+        //
+        void EndOfTurn() 
+        {
+            Player *winner = game->checkForWinner();
+            if (winner)
+            {
+                gameOver = true;
+                gameWinner = winner->playerNumber();
+            }
+            if (game->checkForDraw()) {
+                gameOver = true;
+                gameWinner = -1;
+            }
+        }
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+Using the provided code that can be downloaded from this github add a new class that inherits from game.cpp in the same way TicTacToe.cpp does and implement a working version of the game Connect 4. The game should be added as a fourth choice from the menu so the choices are Tic Tac Toe, Checkers, Othello and now Connect 4.
+
+The game must be playable by both by 2 people and vs. an AI. Your implementation must check for the winning condition, and display that on the right hand side the same way the current setup does for tic-tac-toe. The stopGame() method is expected to clean up and delete any memory allocated.
+
+Look at the new Grid.cpp class and see how it is used in Checkers.cpp for a good understanding about laying out a useable grid.
+
+For extra credit, when a game piece is added to the board, make it animate into place instead of just appearing in it's final spot.
+
+Graphic files are provided for the game pieces called yellow.png and red.png.
+
+For the submission, create a new github based on the above code-base and provide a link to that along with a complete readme.md on how your implementation works.
