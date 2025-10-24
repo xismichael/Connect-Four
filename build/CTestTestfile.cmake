f89343dc91a30cc07abc6f1b204bb090d1aa5166
Michael Xi <ximichael26@gmail.com>
2025-10-24 13:28:52 -0700

    extra files

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/michaelxi/Projects/CMPM123/ConnectFour
+# Build directory: /Users/michaelxi/Projects/CMPM123/ConnectFour/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
